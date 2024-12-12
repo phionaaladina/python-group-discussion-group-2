94fc33eef831a059f50579cc6c2ae455a8dd255e
Phiona Aladina <phionaaladina@gmail.com>
2024-12-12 14:25:35 +0300

    Add Question file

--- oop.py
+++ oop.py
@@ -1,25 +1,21 @@
-'''
 
-Object-Oriented Programming (OOP)
-Basic: Create a class called Car with attributes brand and color. Instantiate an object of the Car class and print its attributes.
-Intermediate: Add a method called start_engine to the Car class that prints a message saying the engine of the car has started. Create an instance of Car and call the method.
-Advanced: Create a class called BankAccount with attributes account_number and balance. Add methods to:
-Deposit an amount.
-Withdraw an amount (only if sufficient balance exists).
-Print the account balance.
-Challenge: Implement a class called Library that manages a collection of books. Each book has a title, author, and available status. The Library class should have methods to:
-Add a book to the library.
-Remove a book from the library.
-Check if a book is available by title.
-Borrow a book (mark it as unavailable if it’s available).
-Return a book (mark it as available again if it was borrowed).
-'''
+
 
 '''
 Basic: Create a class called Car with attributes brand and color. 
 Instantiate an object of the Car class and print its attributes.
 
 '''
+class Car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+my_car = Car('Audi', 'Red')
+print('Car Details:')
+print(f'Brand:{my_car.brand}')
+print(f'Color:{my_car.color}')
+
 
 '''
 Intermediate: Add a method called start_engine to the Car class that prints a message saying the 
@@ -94,7 +90,7 @@ class Book:
         self.is_available = True
 
     def __str__(self):
-        print(f'{self.title} by {self.author}')
+     return f"{self.title} by {self.author}"
 
 
 
